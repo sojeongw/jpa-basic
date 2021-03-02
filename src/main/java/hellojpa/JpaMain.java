e52048ef0ca24cb52e945794e78b4ab00c05b279
dodeon <sojeongw.official@gmail.com>
2021-03-03 06:41:02 +0900

    객체 지향 쿼리 언어: 기본 문법

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,45 +22,9 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member = new Member();
-      member.setUsername("member1");
-      member.setHomeAddress(new Address("home city", "street", "12345"));
-
-      member.getFavoriteFoods().add("치킨");
-      member.getFavoriteFoods().add("족발");
-      member.getFavoriteFoods().add("피자");
-
-      member.getAddressHistory().add(new Address("old1", "street", "12345"));
-      member.getAddressHistory().add(new Address("old2", "street", "12345"));
-
-      em.persist(member);
-
-      // DB에는 데이터가 있고 애플리케이션을 아예 처음부터 시작하는 것처럼 실행할 수 있다.
-      em.flush();
-      em.clear();
-
-      System.out.println("=====================");
-      Member findMember = em.find(Member.class, member.getId());
-
-      // 값 타입은 immutable 해야 하기 때문에 이렇게 변경하면 절대 안된다.
-//      findMember.getHomeAddress().setCity("new city");
-
-      // address 인스턴스 자체를 갈아끼워야 한다.
-      Address a = findMember.getHomeAddress();
-      findMember.setHomeAddress(new Address("new city", a.getStreet(), a.getZipcode()));
-
-      // 단순 String이기 때문에 지우고 넣어야 한다.
-      // String 자체가 값 타입이기 때문에 통으로 갈아 끼우는 것 말고는 방법이 없다. 따로 업데이트가 불가하다.
-      findMember.getFavoriteFoods().remove("치킨");
-      findMember.getFavoriteFoods().add("한식");
-
-      // address를 하나만 바꾸고 싶다면
-      // 기본적으로 컬렉션은 대부분 대상을 찾을 때 equals()로 동작한다.
-      // 그래서 찾고 싶은 값을 그대로 넣어주면 그 값을 찾아준다.
-      // 따라서 equals()를 재정의 하지 않았다면 그냥 망하는 것이다. equals()를 꼭 재정의 해주자.
-      findMember.getAddressHistory().remove(new Address("old1", "street", "12345"));
-      // 지운 값 대신 새로운 값을 넣어준다.
-      findMember.getAddressHistory().add(new Address("new city", "street", "12345"));
+      List<Member> resultList = em
+          .createQuery("select m from Member m where m.username like '%kim%'", Member.class)
+          .getResultList();
 
       tx.commit();
     } catch (Exception e) {
